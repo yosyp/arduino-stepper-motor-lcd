e5651f8057d3db09ca4d678b510b32afdfd668e9
yosyp <y.shvab+git@gmail.com>
2018-03-14 11:05:39 -0400

    [broken] removed LCD and analog pin code

--- main.ino
+++ main.ino
@@ -12,48 +12,18 @@
 
 #include <AccelStepper.h>
 #include <Adafruit_MotorShield.h>
-#include <LiquidCrystal.h>
-#include <ResponsiveAnalogRead.h>
 #include <Wire.h>
 
 #define MAX_POTS 2
 FILE serial_stdout;
 
-/* LCD pins depend on physical configuration */
-LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
-
-/* Analog IO pins */
-int PotentiometerPin[] = {A1, A0}; // Potentiometer values (analog)
-
-/* Placeholders to store calibrated min/max of potentiometers */
-int potMax[] = {1, 1};
-int potMin[] = {100, 100};
-
-/* */
-int PotRead[MAX_POTS];
 float stepperSpeed[MAX_POTS];
 
-/* Digital IO pins */
-int SysSwitch = 6; // Pin for system toggle switch, motors on/off (digital)
-int DirSwitch = 5; // Pin for direction toggle switch (digital)
-
-int direction = 0; // Will store rotation direction of two motors, passed to
-                   // void printArrows()
-
-int SysState, DirState; // Default state for system is off, motors not moving.
-int SysPrev = HIGH,
-    DirPrev = LOW; // Default direction for two motors is both forward.
-
-int SysRead, DirRead;
-int calibrationTime = 10000; // Startup calibration time before main loop (ms)
-
 float stepperSteps = 200.0; // Physical value of the stepper motor
-float max_v = 10000.0;    // Maximum stepper motor velocity (steps)
-float max_a = 10000.0;    // Maximum stepper motor acceleration (steps)
+float max_v = 10000.0;      // Maximum stepper motor velocity (steps)
+float max_a = 10000.0;      // Maximum stepper motor acceleration (steps)
 
 long int timeout = calibrationTime + 1000;
-
-ResponsiveAnalogRead *pot[MAX_POTS];
 
 Adafruit_MotorShield AFMSbot(0x61);
 Adafruit_MotorShield AFMStop(0x60);
@@ -73,58 +43,6 @@ void backwardStep1() { myStepper[1]->onestep(BACKWARD, DOUBLE); }
 AccelStepper *stepper[MAX_POTS];
 
 void loop() {
-  
-  // Calibrate the potentiometers by manually finding min/max values
-  while (millis() < calibrationTime) {
-    calibratePotentiometers();
-  }
-
-  /*
-   * Read the current system state (motors ON or OFF), and
-   * switch state accordingly. Save previous state to compare.
-   */
-  SysRead = digitalRead(SysSwitch);
-  if (SysRead == HIGH && SysPrev == LOW) {
-    SysState = LOW;
-    lcd.setCursor(5, 0);
-    lcd.print(" STOP");
-  } else if (SysRead == LOW && SysPrev == HIGH) {
-    SysState = HIGH;
-    lcd.setCursor(5, 0);
-    lcd.print("  ON  ");
-  }
-  SysPrev = SysRead;
-
-  /*
-   * Read the current motor direction state (can be either
-   * both in the same direction, or rotate in opposite
-   * directions). Switch states accordingly and save previous
-   * state to compare.
-   */
-  DirRead = digitalRead(DirSwitch);
-  if (DirRead == HIGH && DirPrev == LOW) {
-    DirState = LOW;
-    direction = 0;
-  } else if (DirRead == LOW && DirPrev == HIGH) {
-    DirState = HIGH;
-    direction = 1;
-  }
-  DirPrev = DirRead;
-
-  /* int sys = digitalRead(6); */
-
-  /* for (int i=0; i<2; i++) { */
-  /*   if (stepper[i]->distanceToGo() == 0) { */
-  /*     stepper[i]->setCurrentPosition(0); */
-  /*     stepper[i]->moveTo(100); */
-  /*   } */
-  /*   if (sys == HIGH) { */
-  /*     stepper[i]->run(); */
-  /*   } else { */
-  /*     stepper[i]->setSpeed(0); */
-  /*   } */
-  /* } */
-
 
   for (int i = 0; i < MAX_POTS; i++) {
     /*
@@ -139,20 +57,21 @@ void loop() {
      * Values are cutoff to be between 0 and 200.
      */
     // Remaps min/max potentiometer values to stepper steps
-    PotRead[i] = map(analogRead(PotentiometerPin[i]), potMin[i], potMax[i], 1, max_v);
+    PotRead[i] =
+        map(analogRead(PotentiometerPin[i]), potMin[i], potMax[i], 1, max_v);
     //    PotRead[i] = pot[i]->getValue();
     // In case sensor value is out of range, constrain it.
     PotRead[i] = constrain(PotRead[i], 1, (int)max_v);
     PotRead[i] = round_up(PotRead[i], 5);
-    
+
     // speed() returns steps/second, converted to RPM:
     stepperSpeed[i] = ((float)PotRead[i] / (float)stepperSteps) * 60.0;
 
     if (SysState == LOW) {
       stepper[i]->setSpeed(0);
     } else {
-     stepper[i]->setSpeed(PotRead[i]);
-     //      stepper[i]->setSpeed(10000.0);      
+      stepper[i]->setSpeed(PotRead[i]);
+      //      stepper[i]->setSpeed(10000.0);
       if (stepper[i]->distanceToGo() == 0) {
         stepper[i]->setCurrentPosition(0);
         stepper[i]->moveTo(100);
@@ -163,92 +82,18 @@ void loop() {
     if (PotRead[i] <= 1) {
       stepper[i]->setSpeed(0);
     }
-   }
-  
-  /* TODO:
-   *  fix this using a struct to store positions of cursors for each
-   *  stepper
-   */
-    /* lcd.setCursor(0, 1); lcd.print("    "); */
-    /* lcd.setCursor(0, 1); lcd.print(PotRead[1]); */
-    /* lcd.setCursor(9, 1); lcd.print("    "); */
-    /* lcd.setCursor(9, 1); lcd.print(PotRead[0]); */
+  }
 
-  /* printArrows(direction); */
 
-  if (millis()%250 == 0) {
-    for (int i=0; i<MAX_POTS; i++) {
+
+  if (millis() % 250 == 0) {
+    for (int i = 0; i < MAX_POTS; i++) {
       printf("Time:%ld POT:%d Min:%d Max:%d CurrentPotValue:%d CurrentSpeed:",
-             millis(), i,potMin[i], potMax[i], PotRead[i]);
+             millis(), i, potMin[i], potMax[i], PotRead[i]);
       Serial.print(stepper[i]->speed());
       printf("\n");
     }
     //    timeout = millis() + 2000;
   }
-  
 }
 
-/*
- *
- */
-void calibratePotentiometers() {
-  lcd.setCursor(0, 0);
-  lcd.print("CALIBRATION");
-  lcd.setCursor(12,0);
-  lcd.print( (int) ((calibrationTime - millis())/1000) );
-
-  for (int i = 0; i < 2; i++) {
-    PotRead[i] = analogRead(PotentiometerPin[i]);
-  }
-
-  for (int i = 0; i < MAX_POTS; i++) {
-    if (PotRead[i] > potMax[i]) {
-      potMax[i] = PotRead[i];
-    }
-    if (PotRead[i] < potMin[i]) {
-      potMin[i] = PotRead[i];
-    }
-  }
-  lcd.setCursor(5, 0);
-  lcd.print("      "); // Cleanup the "CALIBRATION" message left over.
-}
-
-/*
- * Print direction arrows on first row
- * 0 -- motors spin in same direction, [->- ->-]
- * 1 -- right motor spins in opposite direction, [->- -<-]
- */
-void printArrows(int direction) {
-  int ttime = millis();
-  if ((ttime / 300) % 2 == 0) {
-    if (direction == 0) {
-      lcd.setCursor(0, 0);
-      lcd.print(">->->");
-      lcd.setCursor(11, 0);
-      lcd.print("->->-");
-    } else {
-      lcd.setCursor(0, 0);
-      lcd.print(">->->");
-      lcd.setCursor(11, 0);
-      lcd.print("-<-<-");
-    }
-  } else {
-    if (direction == 0) {
-      lcd.setCursor(0, 0);
-      lcd.print("->->-");
-      lcd.setCursor(11, 0);
-      lcd.print(">->->");
-    } else {
-      lcd.setCursor(0, 0);
-      lcd.print("->->-");
-      lcd.setCursor(11, 0);
-      lcd.print("<-<-<");
-    }
-  }
-  lcd.setCursor(7, 1);
-  if ((ttime / 500) % 2 == 0) {
-    lcd.print("<>");
-  } else {
-    lcd.print("><");
-  }
-}
--- setup.ino
+++ setup.ino
@@ -1,10 +1,11 @@
-int serial_putchar(char c, FILE* f) {
-  if (c == '\n') serial_putchar('\r', f);
-  return Serial.write(c) == 1? 0 : 1;
+int serial_putchar(char c, FILE *f) {
+  if (c == '\n')
+    serial_putchar('\r', f);
+  return Serial.write(c) == 1 ? 0 : 1;
 }
 
 void setup() {
-   Serial.begin(57600);
+  Serial.begin(57600);
   //  TWBR = ((F_CPU / 400000l) - 16) / 2; // Change the i2c clock to 400KHz
 
   fdev_setup_stream(&serial_stdout, serial_putchar, NULL, _FDEV_SETUP_WRITE);
@@ -14,45 +15,16 @@ void setup() {
   /* Should use lambda functions here for campactness, but for readability: */
   stepper[0] = new AccelStepper(forwardStep0, backwardStep0);
   stepper[1] = new AccelStepper(forwardStep1, backwardStep1);
-  
+
   for (int i = 0; i < MAX_POTS; i++) {
-    printf("Setup %d!\n",i);
-    pot[i] = new ResponsiveAnalogRead(PotentiometerPin[i], true);
+    printf("Setup %d!\n", i);
     stepper[i]->setMaxSpeed(max_v);
     stepper[i]->setAcceleration(max_a);
     stepper[i]->moveTo(14000000);
   }
 
-
   AFMSbot.begin(); // Start the bottom shield
   AFMStop.begin(); // Start the top shield
   //  TWBR = ((F_CPU / 400000l) - 16) / 2; // Change the i2c clock to 400KHz
 
-  pinMode(SysSwitch, INPUT);           // Global motor on/off switch
-  pinMode(DirSwitch, INPUT);           // Motor direction switch
-  pinMode(PotentiometerPin[0], INPUT); // Left potentiometer knob
-  pinMode(PotentiometerPin[1], INPUT); // Right potentiometer knob
-
-  /*
-   * Set up the LCD's number of columns and rows.
-   * LCD Layout is as follows:
-   * ******************************************************
-   *  COL: 0  1  2  3  4  5  6  7  8  9  10 11 12 13 14 15
-   * ROW0: -  >  -  >  -     S  T  O  P     -  >  -  >  -
-   * ROW1: L  :  x  x  R  P  M  /  /  R  :  x  x  R  P  M
-   * ******************************************************
-   */
-  lcd.begin(16, 2);
-  lcd.setCursor(0, 1);
-  lcd.print("L");
-  lcd.setCursor(9, 1);
-  lcd.print("R");
-  lcd.setCursor(4, 1);
-  lcd.print("rpm");
-  lcd.setCursor(13, 1);
-  lcd.print("rpm");
-}
-
-float round_up(float num, float factor) {
-  return num + factor - 1 - ((int)num - 1) % (int)factor;
 }
