d99fac23f410ab8ac103a1b2c570495083425ae0
yosyp <y.shvab+git@gmail.com>
2018-04-19 19:39:48 -0400

    skeleton file

---
+++ pushbuttons/main/main.ino
@@ -0,0 +1,30 @@
+/*
+  Buttons PINOUT:
+  4   Stepper 1 increase speed (up)
+  5   Stepper 1 decrease speed (down)
+  6   Stepper 2 increase speed (up)
+  7   Stepper 2 decrease speed (down)    
+  A3  Stepper 3 increase speed (up) addressed at 17
+  A1  Stepper 3 decrease speed (down) addressed as 15
+  A2  Stepper 4 increase speed (up) addressed at 16
+  A0  Stepper 4 decrease speed (down) addressed at 14
+*/
+#include <LiquidCrystal.h>
+#include <AccelStepper.h>
+#include <Adafruit_MotorShield.h>
+#include <Wire.h>
+
+void setup()
+{
+
+}
+
+void loop()
+{
+
+}
+
+
+
+
+
